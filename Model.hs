114b964a968548c8b1e745f3e6675144bb78b14b
cutsea110 <cutsea110@gmail.com>
2015-09-29 06:12:20 +0900

    Define userName

--- Model.hs
+++ Model.hs
@@ -3,9 +3,16 @@ module Model where
 import ClassyPrelude.Yesod
 import Database.Persist.Quasi
 
+import Data.Text as T
+
 -- You can define all of your database entities in the entities file.
 -- You can find more information on persistent and how to declare entities
 -- at:
 -- http://www.yesodweb.com/book/persistent/
 share [mkPersist sqlSettings, mkMigrate "migrateAll"]
     $(persistFileWith lowerCaseSettings "config/models")
+
+userName :: User -> Text
+userName u = if T.null $ userFamilyName u <> userGivenName u
+             then "(no name)"
+             else userFamilyName u <> " " <> userGivenName u
