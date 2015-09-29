82174b26aad2e20166234d94505542ca3705f3cb
cutsea110 <cutsea110@gmail.com>
2015-09-29 12:36:39 +0900

    Define main models

--- Model.hs
+++ Model.hs
@@ -5,6 +5,8 @@ import Database.Persist.Quasi
 
 import Data.Text as T
 
+import Model.Fields
+
 -- You can define all of your database entities in the entities file.
 -- You can find more information on persistent and how to declare entities
 -- at:
