727c492e78d4ea6ed4269e4f6382a7e54436bd3c
cutsea110 <cutsea110@gmail.com>
2016-02-24 19:42:30 +0900

    redirect MyTaskR

--- Handler/Top.hs
+++ Handler/Top.hs
@@ -3,8 +3,4 @@ module Handler.Top where
 import Import
 
 getTopR :: Handler Html
-getTopR = do
-  mu <- maybeAuthId
-  defaultLayout $ do
-    setTitleI MsgTop
-    $(widgetFile "top")
+getTopR = redirect MyTasksR
